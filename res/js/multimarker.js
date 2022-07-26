82594d4eeff2653b1424a69f1bf4c9c64a9e160b
Stanley98745 <35321186+stanley98745@users.noreply.github.com>
2022-07-26 17:12:31 +0800

    Update multimarker.js

--- res/js/multimarker.js
+++ res/js/multimarker.js
@@ -1,7 +1,7 @@
 // We're going to register a custom event listener through a-frame that will fire
 // whenever a marker has entered the camera view and is found through ar.js
 
-AFRAME.registerComponent('videoEvent0', {
+AFRAME.registerComponent('registerevents', {
             init: function() {
                 var marker = this.el;
 

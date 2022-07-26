6dcab6ad381fa0426772ca757fa9a5d26b82a330
Stanley98745 <35321186+stanley98745@users.noreply.github.com>
2022-07-26 16:06:23 +0800

    Update multimarker.js

--- res/js/multimarker.js
+++ res/js/multimarker.js
@@ -1,7 +1,35 @@
 // We're going to register a custom event listener through a-frame that will fire
 // whenever a marker has entered the camera view and is found through ar.js
 
-AFRAME.registerComponent('registerevents', {
+AFRAME.registerComponent('videoEvent0', {
+            init: function() {
+                var marker = this.el;
+
+                // Element emits events when found and lost
+                marker.setAttribute('emitevents', 'true');
+
+                marker.addEventListener('markerFound', function() {
+                    // Alright, a marker has been found. Let's get the video element
+                    var vid = document.getElementById('Video0');
+
+                    // Make sure that the video a-frame object is visible
+                    document.querySelector('#vid0').setAttribute('visible', true);
+
+                    // Reset the video to the beginning and play it through
+                    vid.currentTime = 0;                    
+                    vid.play();
+                    
+                    // Once the video has completed, we're going to hide the a-video element
+                    // which will display the a-image element with the watch behind it
+                    vid.addEventListener('ended', function(e) {
+                        document.querySelector('#vid0').setAttribute('visible', false);
+                    });
+                });
+            }
+        });
+
+
+AFRAME.registerComponent('videoEvent1', {
             init: function() {
                 var marker = this.el;
 

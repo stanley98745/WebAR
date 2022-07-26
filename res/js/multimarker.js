024848940055f3b065abc2112ffd4141e0816d8c
Stanley98745 <35321186+stanley98745@users.noreply.github.com>
2022-07-26 16:12:14 +0800

    Update multimarker.js

--- res/js/multimarker.js
+++ res/js/multimarker.js
@@ -55,3 +55,31 @@ AFRAME.registerComponent('videoEvent1', {
                 });
             }
         });
+
+
+AFRAME.registerComponent('videoEvent2', {
+            init: function() {
+                var marker = this.el;
+
+                // Element emits events when found and lost
+                marker.setAttribute('emitevents', 'true');
+
+                marker.addEventListener('markerFound', function() {
+                    // Alright, a marker has been found. Let's get the video element
+                    var vid = document.getElementById('Video1');
+
+                    // Make sure that the video a-frame object is visible
+                    document.querySelector('#vid2').setAttribute('visible', true);
+
+                    // Reset the video to the beginning and play it through
+                    vid.currentTime = 0;                    
+                    vid.play();
+                    
+                    // Once the video has completed, we're going to hide the a-video element
+                    // which will display the a-image element with the watch behind it
+                    vid.addEventListener('ended', function(e) {
+                        document.querySelector('#vid2').setAttribute('visible', false);
+                    });
+                });
+            }
+        });
